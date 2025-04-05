5b50e70e542f7adf3c56dc43b233609dadc329ef
Jinhyeok Choi <90018372+haedam19@users.noreply.github.com>
2025-04-06 07:49:10 +0900

    refactor: recommender/recommender.py

--- loopin_ai/recommender/recommender.py
+++ loopin_ai/recommender/recommender.py
@@ -1,26 +1,19 @@
 import os
-import tiktoken
 import json
+import logging
 from pathlib import Path
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.prompts import MessagesPlaceholder
 from langchain_openai import ChatOpenAI
-from langchain_openai import OpenAI
 from dotenv import load_dotenv
 
 DAYS = {"MON": "월요일", "TUE": "화요일", "WED": "수요일", "THU": "목요일", "FRI": "금요일", "SAT": "토요일", "SUN": "일요일"}
 
-load_dotenv(override=True)
-print(f"[API KEY]\n{os.environ['OPENAI_API_KEY']}\n")
+SYS_PROMPT_MSG = "당신은 모임 애플리케이션에서 사용자에게 모임을 추천하는 AI입니다."
 
-llm = ChatOpenAI(model="gpt-4o-mini")
-
-def create_template_message(json):
-    msg_list = []
-    msg_list.append(SystemMessage("당신은 모임 애플리케이션에서 사용자에게 모임을 추천하는 AI입니다."))
-    msg_list.append(HumanMessage(f"""모임 {len(json)}개의 정보를 보여드리겠습니다.
+DATA_KNOWLEDGE_DESCRIPTION = """모임 {n}개의 정보를 보여드리겠습니다.
 각 모임 정보는 아래와 같은 항목들로 구성되어 있습니다.
 
 1. ID: 모임 ID                    
@@ -30,7 +23,28 @@ def create_template_message(json):
 5. Description: 모임 설명
 6. Date/Time: 모임 요일 및 시간
 7. Location: 모임 장소
-8. Maxmember: 최대 인원 수 """))
+8. Maxmember: 최대 인원 수"""
+
+TASK_DESCRIPTION = """이상으로 제가 원하는 모임에 대해 설명했습니다.
+이제 다음 절차에 따라 모임을 추천해주세요.
+
+1. 위에서 설명한 모임들 중 제가 원하는 모임에 해당할 것 같은 모임을 최대 5개 선택하세요.
+2. 선택된 모임의 ID 값들을 콤마(,)로 구분해 출력해주세요.
+3. 모임 ID 값 외에 다른 정보는 출력하지 마세요.
+4. 만약 제가 원하는 모임이 없다면 '없음'이라고 출력해주세요.
+출력 예시 1) 1, 3, 4, 7, 9
+출력 예시 2) 3, 8
+출력 예시 3) 없음"""
+
+load_dotenv(override=True)
+logging.debug(f"API KEY: {os.environ.get('OPENAI_API_KEY')[:4]}****")
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+def create_template_message(json):
+    msg_list = []
+    msg_list.append(SystemMessage(SYS_PROMPT_MSG))
+    msg_list.append(HumanMessage(DATA_KNOWLEDGE_DESCRIPTION.format(n = len(json))))
     
     for club in json:
         day_and_time = club['whenMeet'] # 비어있으면 None
@@ -44,35 +58,23 @@ def create_template_message(json):
             time = day_and_time[len(day_and_time) - 1].split(" ")
             for elem in day_list:
                 day_time_string += DAYS[elem] + ", "
-            day_time_string += time[0][:2] + ":" + time[0][2:] + " - " + time[1][:2] + ":" + time[1][2:]
+            day_time_string += time[0][:2] + ":" + time[0][2:] + " - "+ time[1][:2] + ":" + time[1][2:]
         else:
             day_time_string = "미정"
             
-
-        input_msg = f"""1. ID = {club['id']}
-2. Name: {club['name']}
-3. Category: {club['category']}
-4. Subcategory: {club['subCategory']}
-5. Description: {club['description']}
-6. Date/Time: {day_time_string}
-7. Location: {club['whereMeet']}
-8. Maxmember: {club['maximumMember']}
-"""
+        input_msg = (f"1. ID = {club['id']}\n"
+                     f"2. Name: {club['name']}\n"
+                     f"3. Category: {club['category']}\n"
+                     f"4. Subcategory: {club['subCategory']}\n"
+                     f"5. Description: {club['description']}\n"
+                     f"6. Date/Time: {day_time_string}\n"
+                     f"7. Location: {club['whereMeet']}\n"
+                     f"8. Maxmember: {club['maximumMember']}")
         msg_list.append(HumanMessage(content = input_msg))
         
-    msg_list.append(HumanMessage("""이상으로 모든 모임에 대한 정보를 설명했습니다.
-이제 제가 원하는 모임에 대해 설명하겠습니다."""))
+    msg_list.append(HumanMessage("이상으로 모든 모임에 대한 정보를 설명했습니다. 이제 제가 원하는 모임에 대해 설명하겠습니다."))
     msg_list.append(MessagesPlaceholder("UserInput"))
-    msg_list.append(HumanMessage("""이상으로 제가 원하는 모임에 대해 설명했습니다.
-이제 다음 절차에 따라 모임을 추천해주세요.
-                                 
-1. 위에서 설명한 모임들 중 제가 원하는 모임에 해당할 것 같은 모임을 최대 5개 선택하세요.
-2. 선택된 모임의 ID 값들을 콤마(,)로 구분해 출력해주세요.
-3. 모임 ID 값 외에 다른 정보는 출력하지 마세요.
-4. 만약 제가 원하는 모임이 없다면 '없음'이라고 출력해주세요.
-출력 예시 1) 1, 3, 4, 7, 9
-출력 예시 2) 3, 8
-출력 예시 3) 없음"""))
+    msg_list.append(HumanMessage(TASK_DESCRIPTION))
     return msg_list
 
 def make_recommendation(json: list[dict], req_message: str) -> list:
